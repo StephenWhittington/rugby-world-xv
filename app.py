51f2c38cc1044e836e86f43555ab4848b7da3d57
Ubuntu <ubuntu@ip-172-31-81-101.ec2.internal>
2019-10-29 21:15:40 +0000

    removed env file from repository

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 import os
 from flask import Flask
+from dotenv import load_dotenv
+load_dotenv()
 
 
 
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
 def hello():
     return 'Hello World ....again'
-
-app.config["MONGO_DBNAME"] = 'world_xv'
 
 
 if __name__ == '__main__':
