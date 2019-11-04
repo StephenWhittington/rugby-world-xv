3f043c6202af768dd114df6a39825b161902576f
Ubuntu <ubuntu@ip-172-31-81-101.ec2.internal>
2019-11-04 21:52:31 +0000

    Added materialize navbar with functionality

---
+++ .c9/metadata/preview-https:/341ea64d46624cbc97b2b35a42f644a1.vfs.cloud9.us-east-1.amazonaws.com
@@ -0,0 +1,1 @@
+{"changed":false,"filter":false,"title":"[P] https://341ea64d46624cbc97b2b35a42f644a1.vfs.cloud9.us-east-1.amazonaws.com","value":"","undoManager":{"mark":-1,"position":-1,"stack":[]},"preview":{"path":"https://341ea64d46624cbc97b2b35a42f644a1.vfs.cloud9.us-east-1.amazonaws.com","previewer":"previewBrowser","disableInjection":true}}
--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 from dotenv import load_dotenv
 load_dotenv()
 
-
-
-
 app = Flask(__name__)
+
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+
+mongo = PyMongo(app)
 
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ....again'
+@app.route('/view_team')
+def view_team():
+    return render_template("base.html",
+                            team=mongo.db.team.find())
 
 
 if __name__ == '__main__':
