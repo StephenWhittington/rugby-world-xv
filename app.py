f01d07357d2117e1ce17b145f68bcf0c87248ee5
Ubuntu <ubuntu@ip-172-31-81-101.ec2.internal>
2019-11-11 20:27:44 +0000

    Added a new page for the creating of the team

--- app.py
+++ app.py
@@ -14,21 +14,32 @@ mongo = PyMongo(app)
 
 
 @app.route('/')
-@app.route('/view_team')
+@app.route('/create_team')
 def view_team():
     return render_template("base.html",
                             team=mongo.db.team.find())
                             
 @app.route('/new_team')
 def new_team():
-    return render_template("newteam.html",
+    return render_template("addplayers.html",
                             players=mongo.db.players.find())
 
 @app.route('/add_player', methods=['POST'])
 def add_player():
     players =  mongo.db.players
     players.insert_one(request.form.to_dict())
-    return redirect(url_for('view_team'))
+    return redirect(url_for('create_team'))
+    
+@app.route('/create_team')
+def create_team():
+    return render_template("team.html",
+                            team=mongo.db.team.find())
+                            
+@app.route('/create_team', methods=['POST'])
+def add_team():
+    team =  mongo.db.players
+    team.insert_one(request.form.to_dict())
+    return redirect(url_for('create_team'))
 
 
 if __name__ == '__main__':
