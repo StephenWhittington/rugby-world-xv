8f83cd3f81b1af6b3ebdf976e32ec698277793c5
Ubuntu <ubuntu@ip-172-31-81-101.ec2.internal>
2020-01-24 00:22:35 +0000

    added more to unittests

--- test.py
+++ test.py
@@ -9,32 +9,31 @@ class TestRoutes(unittest.TestCase):
         cls.app = app.test_client()
         
     def test_index_route(self):
-        response = self.app.get('/') 
-        # assert the status code of the response
+        response = self.app.get('/', follow_redirects=True) 
         self.assertEqual(response.status_code, 200) 
 
     def test_create_team_route(self):
-        response = self.app.get('/created/teams')
+        response = self.app.get('/created/teams', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
     
     def test_add_player_route(self):
-        response = self.app.get('/player/new')
+        response = self.app.get('/player/new', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
     
     def test_team_new_route(self):
-        response = self.app.get('/team/new')
+        response = self.app.get('/team/new', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
     
     def test_delete_team_route(self):
-        response = self.app.get('/delete_team/<team_id>')
+        response = self.app.get('/delete_team/<team_id>', follow_redirects=True)
         self.assertEqual(response.status_code, 500)
     
     def test_edit_team_route(self):
-        response = self.app.get('/edit_team/<team_id>')
+        response = self.app.get('/edit_team/<team_id>', follow_redirects=True)
         self.assertEqual(response.status_code, 500)
     
     def test_update_team_route(self):
-        response = self.app.get('/update_team/<team_id>')
+        response = self.app.get('/update_team/<team_id>', follow_redirects=True)
         self.assertEqual(response.status_code, 405)
    
 
